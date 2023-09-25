06aed70e64e157b0c0792926a16e4ac1355be08c
Mari Calmer <superlassig@gmail.com>
2023-09-25 17:07:35 +0200

    feat: add baked lady

--- components/ZeusModel.jsx
+++ components/ZeusModel.jsx
@@ -6,16 +6,13 @@ import { useFrame } from "@react-three/fiber";
 import * as THREE from "three";
 
 function ZeusModel({ position, scale, rotation, scrollYProgress }) {
-  const axesHelper = new THREE.AxesHelper(5);
-  const { scene } = useGLTF("./3dmodels/studio_cube.glb");
+  const axesHelper = new THREE.AxesHelper(50);
+  const { scene } = useGLTF("./3dmodels/rebi_07.glb");
   useFrame((state, delta) => {
     //console.log(-2 * (scrollYProgress.current * 0.3));
     scene.add(axesHelper);
-    state.scene.rotation.set(
-      0,
-      Math.atan(scrollYProgress.current * Math.PI * 2) * 4,
-      0
-    );
+    // state.scene.rotation.set(0, Math.atan(scrollYProgress.current * Math.PI * 2) * 4, 0);
+
     // state.scene.rotation.set(_x, _Y, _z);
     // state.scene.position.set(_x, _Y, _z);
     // state.camera.position.set(_x, _y, _Z);
@@ -24,8 +21,9 @@ function ZeusModel({ position, scale, rotation, scrollYProgress }) {
     // state.scene.position.set(0, -2 * (scrollYProgress.current * 0.3), 0);
     // state.camera.position.set(0, 0, 2 - scrollYProgress.current * 1);
     // state.camera.lookAt(0, 0, 0);
-    state.scene.position.set(0, -2 * (scrollYProgress.current * 0.3), 2);
-    state.camera.position.set(0, -2, 5 - scrollYProgress.current * 1);
+    state.scene.rotation.set(0, Math.atan(scrollYProgress.current * Math.PI * 2) * 4, 0);
+    state.scene.position.set(0, -2 * (scrollYProgress.current * 0.3), 0);
+    state.camera.position.set(0, 0, 2 - scrollYProgress.current * 1);
     state.camera.lookAt(0, 0, 0);
   });
   return (
