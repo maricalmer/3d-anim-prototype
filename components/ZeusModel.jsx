1d20a2efa82c928943646f8b083b4c02fd04bc81
Mari Calmer <superlassig@gmail.com>
2023-10-10 17:57:49 +0200

    feat: new camera from close to far

--- components/ZeusModel.jsx
+++ components/ZeusModel.jsx
@@ -24,6 +24,7 @@ function ZeusModel({ position, scale, rotation, scrollYProgress }) {
     state.scene.rotation.set(0, Math.atan(scrollYProgress.current * Math.PI * 2) * 4, 0);
     state.scene.position.set(0, -2 * (scrollYProgress.current * 0.3), 0);
     state.camera.position.set(0, 0, 2 - scrollYProgress.current * 1);
+    // state.camera.position.set(0.5, 0, 1 + scrollYProgress.current * 1);
     state.camera.lookAt(0, 0, 0);
   });
   return (
